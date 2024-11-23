8f69f115a52a4615a53e3b884d4cfe7085fa08f9
Eduardoaprendizado1 <odraude447+github@gmail.com>
2024-11-23 18:29:27 -0300

    ex 003 e 004

--- submission/003.sh
+++ submission/003.sh
@@ -1,1 +1,5 @@
 # How many new outputs were created by block 123,456?
+BLOCKHASH=$(bitcoin-cli getblockhash 123456)
+BLOCK=$(bitcoin-cli getblock $BLOCKHASH 2)
+OUTPUT_COUNT=$(echo $BLOCK | jq '[.tx[].vout | length] | add')
+echo $OUTPUT_COUNT
