77625fae302c7dab58290d01dd04bee9c8dc499d
Eduardoaprendizado1 <odraude447+github@gmail.com>
2024-12-03 16:15:34 -0300

    ex 006

--- submission/006.sh
+++ submission/006.sh
@@ -1,1 +1,26 @@
-# Which tx in block 257,343 spends the coinbase output of block 256,128?
+# Get source block tx array
+block_hash=$(bitcoin-cli getblockhash 256128)
+mapfile -t TxArrayBlock256728 < <(bitcoin-cli getblock "$block_hash" | jq -r '.tx[]')
+
+# Get coinbase tx from source block
+TxCoinbase=$(bitcoin-cli getrawtransaction "${TxArrayBlock256728[0]}" 2 | jq -r '.vin[].coinbase')
+
+# Get target block tx array
+target_block_hash=$(bitcoin-cli getblockhash 257343)
+mapfile -t ToMatchTargetCoinbaseTxArray < <(bitcoin-cli getblock "$target_block_hash" | jq -r '.tx[]')
+
+for tx in "${ToMatchTargetCoinbaseTxArray[@]}"; do
+    mapfile -t TxPreVout < <(bitcoin-cli getrawtransaction "$tx" 2 | jq -r '.vin[].txid')
+
+    for txid in "${TxPreVout[@]}"; do
+        # Check txid length and skip if not 64 characters
+        [[ ${#txid} -eq 64 ]] || continue
+
+        TxToCompare=$(bitcoin-cli getrawtransaction "$txid" 2 | jq -r '.vin[].coinbase')
+
+        if [[ "$TxToCompare" == "$TxCoinbase" ]]; then
+            echo "$tx"
+            break 2
+        fi
+    done
+done
